5f8547edd31962da10b1caab89beafa1af871a16
a.dodhy <a.dodhy@gmail.com>
2018-02-25 12:15:03 +0100

    - added an example for Direct Eval

---
+++ jalangi2/experiments/direct_eval_example.js
@@ -0,0 +1,14 @@
+
+function parent() {
+  function child1(){
+    var x = 2, y = 4;
+    console.log(eval('function directEval() {console.log("In function g: ",x); return x + y; }'));  // Direct call, uses local scope, result is 6
+
+    return directEval();
+  }
+  return child1();
+}
+
+module.exports = {
+  parent: parent
+}
